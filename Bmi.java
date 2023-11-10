0180e24cf24f76167dc2d83f1149433d1b80b1a5
senakikuchi <s.kikuchi.sys23@morijyobi.ac.jp>
2023-11-10 15:38:04 +0900

    Main.javaからBmi.javaに名前を変更

--- Bmi.java
+++ Bmi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Bmi {
     public static void main(String[] args) {
         for(var i=0;i < 2; i++) {
             System.out.println("i=" + i);
